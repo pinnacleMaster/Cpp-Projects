08e8f9f5b2f7d8f3c5d614338ce7365fa47786d2
Takudzwa Mhonde <tmhonde45@gmail.com>
2018-04-30 21:05:02 -0700

    updated the main.cpp file to allow user interaction - user can choose number of disks on starting peg.

--- Algorithms/Tower Of Hanoi/main.cpp
+++ Algorithms/Tower Of Hanoi/main.cpp
@@ -3,17 +3,13 @@
 #include <vector>
 
 int main(){
-
-	TowerOfHanoi Tower1(1);
+	int n; std::cout << "The number of disks: "; std::cin >> n;
+	TowerOfHanoi Tower1(n);
 	std::vector<std::string> moves1 = Tower1.generateSolution();
 	for (auto move: moves1)
 		std::cout << move << std::endl;
 	std::cout << "There are " << moves1.size() << " moves to make to solve this." << std::endl;
 
-	TowerOfHanoi Tower2(3,'B', 'A');
-	std::vector<std::string> moves2 = Tower2.generateSolution();
-	for (auto move: moves2)
-		std::cout << move << std::endl;
-	std::cout << "There are " << moves2.size() << " moves to make to solve this." << std::endl;
+	return 0;
 	
 }
